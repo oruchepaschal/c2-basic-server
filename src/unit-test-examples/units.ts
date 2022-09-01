dd3d3e7b4d16ac8143f67ebc1b872100255fa140
oruchepaschal <oruchepaschal@gmail.com>
2022-09-01 04:51:38 +0100

    udacity exercise 02

---
+++ src/unit-test-examples/units.ts
@@ -0,0 +1,24 @@
+// Super Simple Unit Functions
+
+export const add = (a: number, b: number) => {
+    return a + b;
+    }
+
+export const divide = (a: number, b: number) => {
+    if(b === 0) { throw new Error('div by 0') }
+
+    return a / b;
+    }
+
+// @TODO try creating a method "concat" to concatenate two strings
+// it should take two string paramaters.
+// it should return one string combining the two strings.
+// it should throw an error if either of the strings are empty.
+// ensure your function is exported.
+
+export const concat = (first_word: string, second_word: string) => {
+    if(first_word === "" || second_word === ""){
+        throw new Error('Cannot concat empty strings')
+    }
+    return first_word + second_word;
+} 
